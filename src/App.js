67f18ab19823bb8be061cac9bc1ff6fe38e5d1a0
vishalgautamm <vishal.g@live.ca>
2017-04-18 22:54:25 -0400

    Installed axios library and changed the componentDidMount function from fetch to axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 import SearchForm from './Components/SearchForm';
 import GifList from './Components/GifList';
 
@@ -13,14 +14,9 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
-      .then(response => response.json())
-      .then(responseData => {
-        this.setState({ gifs: responseData.data })
-      })
-      .catch(error => {
-        console.log('Error fetching and parsing data', error);
-      });
+    axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+      .then(response => this.setState({ gifs: response.data.data }))
+      .catch(error => console.log('Error when fetching and parsing data', error));
   }
 
   render() {
