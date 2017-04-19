6e5b1c1751d1cdfce855efcb876dfff00b47776e
vishalgautamm <vishal.g@live.ca>
2017-04-19 00:57:40 -0400

    Added search functionality

--- src/App.js
+++ src/App.js
@@ -19,6 +19,12 @@ export default class App extends Component {
       .catch(error => console.log('Error when fetching and parsing data', error));
   }
 
+  performSearch = query => {
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
+      .then(response => this.setState({ gifs: response.data.data }))
+      .catch(error => console.log('Error when fetching and parsing data', error));
+  };
+
   render() {
     console.log(this.state.gifs);
     return (
@@ -26,7 +32,7 @@ export default class App extends Component {
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />
+            <SearchForm onSearch={this.performSearch}/>
           </div>
         </div>
         <div className="main-content">
